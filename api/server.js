f65453799569b6ac5ca1403ed860788bf675eeb9
Logan Brock <logan.g.brock@gmail.com>
2021-07-27 11:42:39 -0400

    quick update

--- api/server.js
+++ api/server.js
@@ -29,10 +29,10 @@ app.use('/api/users', userRoute);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('../frontend/build'));
+	app.use(express.static('../client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(_dirname, 'frontend', 'build', 'index.html'));
+		res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
 	});
 }
 
